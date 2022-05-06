4b7fc27e4c345dd92a0813915b93808f4459ac8c
Jose Luis Carrasco <92541048+JoseLuCD@users.noreply.github.com>
2022-05-06 19:02:52 +0200

    rama 2 calculadora

--- WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -37,6 +37,8 @@ namespace WindowsFormsApp1
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.Suma = new System.Windows.Forms.Button();
+            this.Resta = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // Multiplicacion
@@ -111,11 +113,33 @@ namespace WindowsFormsApp1
             this.textBox3.TabIndex = 7;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
+            // Suma
+            // 
+            this.Suma.Location = new System.Drawing.Point(619, 204);
+            this.Suma.Name = "Suma";
+            this.Suma.Size = new System.Drawing.Size(100, 40);
+            this.Suma.TabIndex = 8;
+            this.Suma.Text = "Suma";
+            this.Suma.UseVisualStyleBackColor = true;
+            this.Suma.Click += new System.EventHandler(this.Suma_Click);
+            // 
+            // Resta
+            // 
+            this.Resta.Location = new System.Drawing.Point(619, 273);
+            this.Resta.Name = "Resta";
+            this.Resta.Size = new System.Drawing.Size(100, 39);
+            this.Resta.TabIndex = 9;
+            this.Resta.Text = "Resta";
+            this.Resta.UseVisualStyleBackColor = true;
+            this.Resta.Click += new System.EventHandler(this.Resta_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(845, 455);
+            this.Controls.Add(this.Resta);
+            this.Controls.Add(this.Suma);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.textBox2);
@@ -141,6 +165,8 @@ namespace WindowsFormsApp1
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button Suma;
+        private System.Windows.Forms.Button Resta;
     }
 }
 
--- WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -13,6 +13,7 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
         double x, y;
+        double result;
 
         public Form1()
         {
@@ -26,15 +27,16 @@ namespace WindowsFormsApp1
 
         private void button1_Click(object sender, EventArgs e)
         {
-            x = Convert.ToDouble(textBox1.Text);
-            y = Convert.ToDouble(textBox2.Text);
-            textBox3.Text = string.Format("{0:f2}", x * y);
+            x = double.Parse(textBox1.Text);
+            y = double.Parse(textBox2.Text);
+            result = x * y;
+            textBox3.Text = result.ToString();
         }
         private void Division_Click(object sender, EventArgs e) 
         {
             
-            x = Convert.ToDouble(textBox1.Text);
-            y = Convert.ToDouble(textBox2.Text);
+            x = double.Parse(textBox1.Text);
+            y = double.Parse(textBox2.Text);
             if (y != 0)
             { 
                 double res = x / y;
@@ -51,6 +53,23 @@ namespace WindowsFormsApp1
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        private void Suma_Click(object sender, EventArgs e)
+        {
+            x = double.Parse(textBox1.Text);
+            y = double.Parse(textBox2.Text);
+            result = x + y;
+            textBox3.Text = result.ToString();
+        }
+
+        private void Resta_Click(object sender, EventArgs e)
+        {
+
+            x = double.Parse(textBox1.Text);
+            y = double.Parse(textBox2.Text);
+            result = x - y;
+            textBox3.Text = result.ToString();
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
